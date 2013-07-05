30b84f58de35ac515c73137575e4a9acc67fd3d9
Martin Reinfried <mr@incredible.io>
2013-07-05 13:16:32 -0700

    Added some preliminary code to test socket connection to pi

--- Runway/RWFirstViewController.h
+++ Runway/RWFirstViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RWFirstViewController : UIViewController<UIPickerViewDelegate>
+@interface RWFirstViewController : UIViewController<UIPickerViewDelegate, NSStreamDelegate>
 @property (nonatomic, strong) IBOutlet UIPickerView *patternPicker;
 @property (nonatomic, strong) IBOutlet UIButton *allControlsButton;
 @property (nonatomic, strong) IBOutlet UIButton *lightsButton;
@@ -16,6 +16,7 @@
 @property (nonatomic, strong) IBOutlet UISlider *tempoSlider;
 @property (nonatomic, strong) IBOutlet UIPanGestureRecognizer *panGS;
 @property (nonatomic, strong) IBOutlet UIPanGestureRecognizer *tapGS;
+@property (nonatomic, strong) IBOutlet UIToolbar *topToolbar;
 
 - (IBAction)controlButtonTapped:(id)sender;
 
--- Runway/RWFirstViewController.m
+++ Runway/RWFirstViewController.m
@@ -12,13 +12,39 @@
 
 @end
 
-@implementation RWFirstViewController
-@synthesize patternPicker, allControlsButton, lightsButton, fireButton, tempoSlider, panGS, tapGS;
+@implementation RWFirstViewController {
+    NSInputStream *inputStream;
+    NSOutputStream *outputStream;
+}
+@synthesize patternPicker, allControlsButton, lightsButton, fireButton, tempoSlider, panGS, tapGS, topToolbar;
+
+- (void)initNetworkCommunication {
+    // remove when active
+    return;
+    
+    // use pi server port here
+    uint portNo = 5555;
+    CFReadStreamRef readStream;
+    CFWriteStreamRef writeStream;
+    // use pi server ip here
+    CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)@"227.3.4.56", portNo, &readStream, &writeStream);
+    inputStream = (__bridge NSInputStream *)readStream;
+    outputStream = (__bridge NSOutputStream *)writeStream;
+    
+    [inputStream setDelegate:self];
+    [outputStream setDelegate:self];
+    
+    [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+    [inputStream open];
+    [outputStream open];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self initNetworkCommunication];
 }
 
 - (void)didReceiveMemoryWarning
@@ -29,11 +55,66 @@
 
 - (void)controlButtonTapped:(id)sender {
     // change images based on which controls are active
+   
+    // coopting this for the moment for testing of socket server
+    NSString *dataString  = @"Hello World!";
+    NSData *data = [[NSData alloc] initWithData:[dataString dataUsingEncoding:NSASCIIStringEncoding]];
+    [outputStream write:[data bytes] maxLength:[data length]];
 }
 
 #pragma mark - UIPickerViewDelegate
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
     // respond to pattern choice
+}
+
+- (void)stream:(NSStream *)theStream handleEvent:(NSStreamEvent)streamEvent {
+    uint8_t buffer[1024];
+    int len;
+    
+    switch (streamEvent) {
+            
+        case NSStreamEventOpenCompleted:
+            NSLog(@"Stream opened now");
+            break;
+        case NSStreamEventHasBytesAvailable:
+            NSLog(@"has bytes");
+            if (theStream == inputStream) {
+                while ([inputStream hasBytesAvailable]) {
+                    len = [inputStream read:buffer maxLength:sizeof(buffer)];
+                    if (len > 0) {
+                        
+                        NSString *output = [[NSString alloc] initWithBytes:buffer length:len encoding:NSASCIIStringEncoding];
+                        
+                        if (nil != output) {
+                            NSLog(@"server said: %@", output);
+                        }
+                    }
+                }
+            } else {
+                NSLog(@"it is NOT theStream == inputStream");
+            }
+            break;
+        case NSStreamEventHasSpaceAvailable:
+            NSLog(@"Stream has space available now");
+            break;
+            
+            
+        case NSStreamEventErrorOccurred:
+            NSLog(@"Can not connect to the host!");
+            break;
+            
+            
+        case NSStreamEventEndEncountered:
+            
+            [theStream close];
+            [theStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+            
+            break;
+            
+        default:
+            NSLog(@"Unknown event %i", streamEvent);
+    }
+    
 }
 
 @end
