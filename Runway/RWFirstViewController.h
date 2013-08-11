0567a6c575da50c6aade9ac7a52178f64143668e
Martin Reinfried <mr@incredible.io>
2013-08-11 09:42:23 -0700

    test patterns, configuration updates, deal with touches directly, etc.

--- Runway/RWFirstViewController.h
+++ Runway/RWFirstViewController.h
@@ -15,20 +15,28 @@
 @property (nonatomic, strong) IBOutlet UIButton *lightsButton;
 @property (nonatomic, strong) IBOutlet UIButton *fireButton;
 @property (nonatomic, strong) IBOutlet UISlider *tempoSlider;
-@property (nonatomic, strong) IBOutlet UIPanGestureRecognizer *panGS;
-@property (nonatomic, strong) IBOutlet UIPanGestureRecognizer *tapGS;
 @property (nonatomic, strong) IBOutlet UIToolbar *topToolbar;
 @property (nonatomic, strong) IBOutlet UIImageView *topImage;
 @property (nonatomic, strong) IBOutlet UIImageView *bottomImage;
 @property (nonatomic, strong) IBOutlet UILabel *tapLabel;
 @property (nonatomic, strong) IBOutlet UISwitch *tapSwitch;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *onBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *offBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *panicBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *pattern1BarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *pattern2BarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *pattern3BarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *pattern4BarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *pattern5BarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *recordBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *stopRecordBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *loopBarButton;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *stopLoopBarButton;
 @property (nonatomic, strong) NSArray *patterns;
 
 - (IBAction)controlButtonTapped:(id)sender;
 - (IBAction)connectButtonTapped:(id)sender;
 - (IBAction)sliderChanged:(id)sender;
-- (IBAction)imageTapped:(UIGestureRecognizer *)sender;
-- (IBAction)imagePanned:(UIGestureRecognizer *)sender;
 
 
 @end
--- Runway/RWFirstViewController.m
+++ Runway/RWFirstViewController.m
@@ -8,17 +8,19 @@
 
 #import "RWFirstViewController.h"
 
-// these will be based on the actual images we use
-#define LIGHTS_PER_SIDE 41
-#define FIRE_PER_SIDE 19
-#define VIEW_ADJUSTMENT 483
+// these will be based on the actual images and configuration we use
+#define LIGHTS_PER_SIDE 42
+#define FIRE_PER_SIDE 20
 #define FIRE_TOP_Y 70
 #define FIRE_BOTTOM_Y 102
 #define LIGHT_TOP_Y 32
 #define LIGHT_BOTTOM_Y 142
 #define LIGHT_WIDTH 10
 #define LIGHT_GAP 14
-#define LIGHT_INITIAL_GAP 27
+#define LIGHT_INITIAL_GAP 15
+
+// change as we hook up lights
+#define LIGHTS_FOR_TEST 19
 
 @interface RWFirstViewController ()
 @property (nonatomic, strong) SRWebSocket *wSocket;
@@ -27,23 +29,37 @@
 @implementation RWFirstViewController {
     BOOL _controllingLights;
     BOOL _controllingFire;
-    BOOL _touchingFire;
-    BOOL _touchingLight;
     NSInteger _numTapped;
     NSDate * _firstTappedTime;
     BOOL _playaMode;
     BOOL _running;
+    BOOL _recordOn;
+    NSTimer *_timer;
+    NSMutableArray *_panTouchingStatus;
 }
-@synthesize patternPicker, allControlsButton, lightsButton, fireButton, tempoSlider, panGS, tapGS, topToolbar, topImage, bottomImage, wSocket, tapLabel, tapSwitch, patterns;
+@synthesize patternPicker, allControlsButton, lightsButton, fireButton, tempoSlider, topToolbar, topImage, bottomImage, wSocket, tapLabel, tapSwitch, onBarButton, offBarButton, panicBarButton, pattern1BarButton, pattern2BarButton, pattern3BarButton, pattern4BarButton, pattern5BarButton, recordBarButton, stopRecordBarButton, loopBarButton, stopLoopBarButton, patterns;
 
+#pragma mark socket functions
 - (void)initNetworkCommunication {
     NSString *urlString = _playaMode ? @"ws://10.0.0.1:8000" : @"ws://raspberrypi.local:8000";
     self.wSocket = [[SRWebSocket alloc] initWithURL:[NSURL URLWithString:urlString]];
-    //self.wSocket = [[SRWebSocket alloc] initWithURL:[NSURL URLWithString:@"ws://10.0.0.1:8000"]];
     self.wSocket.delegate = self;
     [self.wSocket open];
 }
 
+- (BOOL)send:(NSString *)msg {
+    NSLog(@"sending %@...", msg);
+    if (self.wSocket.readyState == SR_OPEN) {
+        [self.wSocket send:msg];
+        return YES;
+    }
+    else {
+        NSLog(@"socket not open: nothing sent");
+        return NO;
+    }
+}
+
+#pragma mark view lifecycle
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -60,12 +76,41 @@
     UITapGestureRecognizer *tempoTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tempoTapped:)];
     [tapLabel addGestureRecognizer:tempoTapRecognizer];
     
-    _playaMode = NO;
+    // init
+    _playaMode = YES;
     _running = NO;
+    _recordOn = NO;
     _numTapped = 0;
     
+    //actions
     [tapSwitch addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
-}
+    
+    // bar buttons
+    self.offBarButton.tintColor = [UIColor blueColor];
+    onBarButton.action = @selector(initNetworkCommunication);
+    offBarButton.action = @selector(disconnect);
+    panicBarButton.action = @selector(panic);
+    pattern1BarButton.action = @selector(runPattern:);
+    pattern2BarButton.action = @selector(runPattern:);
+    pattern3BarButton.action = @selector(runPattern:);
+    pattern4BarButton.action = @selector(runPattern:);
+    pattern5BarButton.action = @selector(runPattern:);
+    recordBarButton.action = @selector(recordButtonTapped);
+    
+    // set up array for panning view
+    _panTouchingStatus = [[NSMutableArray alloc] initWithCapacity:2];
+    NSMutableDictionary *view1TouchDict = [[NSMutableDictionary alloc] initWithCapacity:2];
+    NSMutableDictionary *view2TouchDict = [[NSMutableDictionary alloc] initWithCapacity:2];
+    [view1TouchDict setObject:@NO forKey:@"fire"];
+    [view1TouchDict setObject:@NO forKey:@"light"];
+    [_panTouchingStatus addObject:view1TouchDict];
+    [_panTouchingStatus addObject:view2TouchDict];
+    
+    //self.view.multipleTouchEnabled = YES;
+    
+    // timer to allow pi to shut off
+    _timer = [NSTimer timerWithTimeInterval:15 target:self selector:@selector(timerFire:) userInfo:nil repeats:YES];
+}   
 
 - (void)didReceiveMemoryWarning
 {
@@ -73,14 +118,63 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (BOOL)send:(NSString *)msg {
-    if (self.wSocket.readyState == SR_OPEN) {
-        [self.wSocket send:msg];
-        return YES;
+#pragma mark actions
+- (void)timerFire:(NSTimer *)timer {
+    [self send:@"alive=1"];
+}
+
+- (void)disconnect {
+    [self.wSocket close];
+}
+
+- (void)panic {
+    [self send:@"panic=1"];
+}
+
+- (void)recordButtonTapped {
+    _recordOn = YES;
+}
+
+// tests
+- (void)runPattern:(id)sender {
+    NSString *buttonTitle = [(UIBarButtonItem *)sender title];
+    NSTimeInterval delay;
+    // crossover pattern
+    if ([buttonTitle isEqualToString:@"Pattern 1"]) {
+        delay = 0.3;
+        for (NSInteger i=0; i<LIGHTS_FOR_TEST; i++) {
+            NSTimeInterval fullDelay = delay*(i+1);
+            [self performSelector:@selector(send:) withObject:[NSString stringWithFormat:@"light=%d", i+1] afterDelay:fullDelay];
+        }
+        for (NSInteger i=LIGHTS_FOR_TEST-1; i>=0; i--) {
+            NSTimeInterval fullDelay = delay*(LIGHTS_FOR_TEST-i-1);
+            [self performSelector:@selector(send:) withObject:[NSString stringWithFormat:@"light=%d", i+1] afterDelay:fullDelay];
+        }
+        return;
     }
-    else {
-        NSLog(@"socket not open: nothing sent");
-        return NO;
+    // slowish
+    else if ([buttonTitle isEqualToString:@"Pattern 2"]) {
+        delay = 0.2;
+    }
+    // fastish
+    else if ([buttonTitle isEqualToString:@"Pattern 3"]) {
+        delay = 0.1;
+    }
+    // fast
+    else if ([buttonTitle isEqualToString:@"Pattern 4"]) {
+        delay = 0.05;
+    }
+    // blazing
+    else if ([buttonTitle isEqualToString:@"Pattern 5"]) {
+        delay = 0.005;
+    }
+    // do it 8 times so we can spot any glitches
+    for (int j=0; j<8; j++) {
+        for (NSInteger i=0; i<LIGHTS_FOR_TEST; i++) {
+            // complicated but right
+            NSTimeInterval fullDelay = delay*LIGHTS_FOR_TEST*j + delay*(i+1);
+            [self performSelector:@selector(send:) withObject:[NSString stringWithFormat:@"light=%d", i+1] afterDelay:fullDelay];
+        }
     }
 }
 
@@ -94,8 +188,7 @@
     }
     UISlider *slider = (UISlider *)sender;
     CGFloat value = slider.value;
-    NSLog(@"tick=%f", 1.0 - value);
-    [self send:[NSString stringWithFormat:@"tick=%f", 1.0 - value]];
+    [self send:[NSString stringWithFormat:@"tick=%f", 1.1 - value]];
 }
 
 - (void)tempoTapped:(id)sender {
@@ -112,7 +205,6 @@
         CGFloat rate = interval / _numTapped;
         // only send tick after 5 taps
         if (_numTapped >= 5) {
-            NSLog(@"tick=%f", rate);
             [self send:[NSString stringWithFormat:@"tick=%f", rate]];
         }
     }
@@ -121,6 +213,9 @@
 - (void)connectButtonTapped:(id)sender {
     if ([[[(UIButton *)sender titleLabel] text] isEqualToString:@"Connect Playa"]) {
         _playaMode = YES;
+    }
+    else {
+        _playaMode = NO;
     }
     if (!_running) {
         [self initNetworkCommunication];
@@ -149,29 +244,25 @@
     }
 }
 
-- (void)imageTapped:(UIGestureRecognizer *)sender {
-    UIView *view = sender.view;
-    CGPoint location = [sender locationInView:view];
-    // sender's view is always bottom image, so check y value to determine which view was clicked on
-    NSInteger startingLightNum = location.y < 0 ? 0 : LIGHTS_PER_SIDE;
-    NSInteger startingFireNum = location.y < 0 ? 0 : FIRE_PER_SIDE;
-    // adjust y value if clicked on top image
+#pragma mark node calculations
+- (void)sendNodeDataBasedOnTap:(CGPoint)location view:(NSInteger)viewNum {
+    NSInteger startingLightNum = viewNum == 0 ? 1 : LIGHTS_PER_SIDE+1;
+    NSInteger startingFireNum = viewNum == 0 ? 0 : FIRE_PER_SIDE;
+    // no adjustment now
     CGFloat adjustedY = location.y;
-    if (adjustedY < 0) adjustedY += VIEW_ADJUSTMENT;
-    NSLog(@"location:%f,%f", location.x, adjustedY);
     
     BOOL hitFire = NO;
     // fire touch has to be precisely within target
     NSInteger lightCycle = LIGHT_WIDTH + LIGHT_GAP;
-    NSInteger minX = LIGHT_INITIAL_GAP + 2*lightCycle + LIGHT_WIDTH;
-    if (_controllingFire && adjustedY >= FIRE_TOP_Y && adjustedY <= FIRE_BOTTOM_Y && location.x >= minX) {
+    NSInteger minX = LIGHT_INITIAL_GAP + lightCycle + LIGHT_WIDTH;
+    NSInteger maxX = LIGHT_INITIAL_GAP + (LIGHTS_PER_SIDE-1)*lightCycle;
+    if (_controllingFire && adjustedY >= FIRE_TOP_Y && adjustedY <= FIRE_BOTTOM_Y && location.x >= minX && location.x < maxX) {
         NSInteger adjustedX = location.x - LIGHT_INITIAL_GAP;
         NSInteger modX = adjustedX % lightCycle;
         NSInteger divX = adjustedX / lightCycle;
-        if (modX > LIGHT_WIDTH && divX % 2 == 0) {
+        if (modX > LIGHT_WIDTH && divX % 2 == 1) {
             hitFire = YES;
-            NSLog(@"fire=%d", startingFireNum + (divX-2)/2);
-            [self send:[NSString stringWithFormat:@"fire=%d", startingFireNum + (divX-2)/2]];
+            [self send:[NSString stringWithFormat:@"fire=%d", startingFireNum + (divX-1)/2]];
         }
     }
     // if fire wasn't hit, turn on closest light to touch, even if it missed the actual target
@@ -179,85 +270,75 @@
         NSInteger xAdjustemnt = LIGHT_INITIAL_GAP - LIGHT_GAP/2;
         NSInteger adjustedX = location.x - xAdjustemnt;
         if (adjustedX < 0) adjustedX = 0;
-        NSInteger highX = 1024 - xAdjustemnt;
+        NSInteger highX = 1023 - 2*xAdjustemnt;
         if (adjustedX > highX) adjustedX = highX;
-        NSLog(@"light=%d", startingLightNum + adjustedX/lightCycle);
         [self send:[NSString stringWithFormat:@"light=%d", startingLightNum + adjustedX/lightCycle]];
     }
 }
 
-- (void)imagePanned:(UIGestureRecognizer *)sender {
-    // reset on end of pan
-    if (sender.state == UIGestureRecognizerStateEnded) {
-        _touchingLight = NO;
-        _touchingFire = NO;
-        return;
-    }
-    // panning
-    UIView *view = sender.view;
-    CGPoint location = [sender locationInView:view];
+// TO DO: deal with multiple pans on same side and pans from one view to another
+- (void)sendNodeDataBasedOnPan:(CGPoint)location view:(NSInteger)viewNum {
     // sender's view is always bottom image, so check y value to determine which view was clicked on
-    NSInteger startingLightNum = location.y < 0 ? 0 : LIGHTS_PER_SIDE;
-    NSInteger startingFireNum = location.y < 0 ? 0 : FIRE_PER_SIDE;
-    // adjust y value if clicked on top image
+    NSInteger startingLightNum = viewNum == 0 ? 1 : LIGHTS_PER_SIDE+1;
+    NSInteger startingFireNum = viewNum == 0 ? 0 : FIRE_PER_SIDE;
+    // no adjustment anymore
     CGFloat adjustedY = location.y;
-    if (adjustedY < 0) adjustedY += VIEW_ADJUSTMENT;
-    NSLog(@"location:%f,%f", location.x, adjustedY);
+    
+    NSMutableDictionary *touchingDict = [_panTouchingStatus objectAtIndex:viewNum];
     
     NSInteger lightCycle = LIGHT_WIDTH + LIGHT_GAP;
-    NSInteger minX = LIGHT_INITIAL_GAP + 2*lightCycle + LIGHT_WIDTH;
+    NSInteger minX = LIGHT_INITIAL_GAP + lightCycle + LIGHT_WIDTH;
+    NSInteger maxX = LIGHT_INITIAL_GAP + (LIGHTS_PER_SIDE-1)*lightCycle;
     if (_controllingFire) {
-        if (adjustedY >= FIRE_TOP_Y && adjustedY <= FIRE_BOTTOM_Y && location.x >= minX) {
+        if (adjustedY >= FIRE_TOP_Y && adjustedY <= FIRE_BOTTOM_Y && location.x >= minX && location.x < maxX) {
             NSInteger adjustedX = location.x - LIGHT_INITIAL_GAP;
             NSInteger modX = adjustedX % lightCycle;
             NSInteger divX = adjustedX / lightCycle;
             // we're touching fire
-            if (modX > LIGHT_WIDTH && divX % 2 == 0) {
+            if (modX > LIGHT_WIDTH && divX % 2 == 1) {
                 // if we were already touching fire, don't send again
-                if (_touchingFire) {
+                if ([[touchingDict objectForKey:@"fire"] boolValue]) {
                     return;
                 }
                 // if not, send the command
                 else {
-                    _touchingFire = YES;
-                    _touchingLight = NO;
-                    NSLog(@"fire=%d", startingFireNum + (divX-2)/2);
-                    [self send:[NSString stringWithFormat:@"fire=%d", startingFireNum + (divX-2)/2]];
+                    [touchingDict setObject:@YES forKey:@"fire"];
+                    [touchingDict setObject:@NO forKey:@"light"];
+                    [self send:[NSString stringWithFormat:@"fire=%d", startingFireNum + (divX-1)/2]];
                     return;
                 }
             }
             // we're touching nothing, set both to NO
             else if (modX > LIGHT_WIDTH) {
-                _touchingLight = NO;
-                _touchingFire = NO;
+                [touchingDict setObject:@NO forKey:@"light"];
+                [touchingDict setObject:@NO forKey:@"fire"];
                 return;
             }
             // we're touching a light area
             // if we were already touching a light, ignore
-            else if (_touchingLight) {
+            else if ([[touchingDict objectForKey:@"light"] boolValue]) {
                 return;
             }
             // newly touching a light area
             else {
-                _touchingFire = NO;
+                [touchingDict setObject:@NO forKey:@"fire"];
                 // send command if we're controlling lights
                 if (_controllingLights) {
-                    _touchingLight = YES;
-                    NSLog(@"light=%d", startingLightNum + divX);
+                    [touchingDict setObject:@YES forKey:@"light"];
                     [self send:[NSString stringWithFormat:@"light=%d", startingLightNum + divX]];
                     return;
                 }
             }
         }
         // we're out of fire's range
-        else if (_touchingFire) {
-            _touchingFire = NO;
+        else if ([[touchingDict objectForKey:@"fire"] boolValue]) {
+            [touchingDict setObject:@NO forKey:@"fire"];
         }
     }
     if (_controllingLights) {
         // TO DO: fix these numbers
         if (adjustedY < LIGHT_TOP_Y || adjustedY > LIGHT_BOTTOM_Y || location.x < LIGHT_INITIAL_GAP) {
-            _touchingLight = NO;
+            [touchingDict setObject:@NO forKey:@"light"];
         }
         else {
             NSInteger adjustedX = location.x - LIGHT_INITIAL_GAP;
@@ -266,22 +347,21 @@
             // touching light
             if (modX <= LIGHT_WIDTH) {
                 // already touching
-                if (_touchingLight) {
+                if ([[touchingDict objectForKey:@"light"] boolValue]) {
                     return;
                 }
                 // newly touching, send command
                 else {
-                    _touchingLight = YES;
-                    _touchingFire = NO;
-                    NSLog(@"light=%d", startingLightNum + divX);
+                    [touchingDict setObject:@YES forKey:@"light"];
+                    [touchingDict setObject:@NO forKey:@"fire"];
                     [self send:[NSString stringWithFormat:@"light=%d", startingLightNum + divX]];
                     return;
                 }
             }
             // not touching anything
             else {
-                _touchingFire = NO;
-                _touchingLight = NO;
+                [touchingDict setObject:@NO forKey:@"fire"];
+                [touchingDict setObject:@NO forKey:@"light"];
             }
         }
     }
@@ -290,7 +370,6 @@
 #pragma mark - UIPickerViewDelegate
 - (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
     // respond to pattern choice
-    NSLog(@"pattern=%d", row);
     [self send:[NSString stringWithFormat:@"pattern=%d", row]];
 }
 
@@ -314,18 +393,57 @@
 - (void)webSocketDidOpen:(SRWebSocket *)webSocket {
     NSLog(@"webSocket did open");
     _running = YES;
+    self.offBarButton.tintColor = nil;
+    self.onBarButton.tintColor = [UIColor blueColor];
 }
 
 - (void)webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    // this doesn't seem to get called if we call open and can't connect
     NSLog(@"webSocket did fail %@", error);
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Connection Failure" message:@"The app could not connect to the pi" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
 }
 
 - (void)webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
     NSLog(@"webSocket did close %d (%@)", code, reason);
     _running = NO;
+    self.onBarButton.tintColor = nil;
+    self.offBarButton.tintColor = [UIColor blueColor];
     self.tempoSlider.value = 0.5;
     [self.patternPicker selectedRowInComponent:0];
     self.tapSwitch.on = NO;
+}
+
+#pragma mark touches functions
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    if (![touch.view isKindOfClass:[UIImageView class]]) {
+        return;
+    }
+    CGPoint location = [touch locationInView:touch.view];
+    NSInteger viewNum = touch.view.frame.origin.y < 200 ? 0 : 1;
+    [self sendNodeDataBasedOnTap:location view:viewNum];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    if (![touch.view isKindOfClass:[UIImageView class]]) {
+        return;
+    }
+    CGPoint location = [touch locationInView:touch.view];
+    NSInteger viewNum = touch.view.frame.origin.y < 200 ? 0 : 1;
+    [self sendNodeDataBasedOnPan:location view:viewNum];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    if (![touch.view isKindOfClass:[UIImageView class]]) {
+        return;
+    }
+    NSInteger viewNum = touch.view.frame.origin.y < 200 ? 0 : 1;
+    NSMutableDictionary *touchDict = [_panTouchingStatus objectAtIndex:viewNum];
+    [touchDict setObject:@NO forKey:@"fire"];
+    [touchDict setObject:@NO forKey:@"light"];
 }
 
 @end
